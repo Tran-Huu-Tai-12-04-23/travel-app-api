d890b586c8ee344debe3a2918112ac640e7a6832
Tran-Huu-Tai-12-04-23 <huutt201@gmail.com>
2024-05-05 22:49:03 +0700

    fix issue

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-   res.status(400).send('HUUTAI CI/CD');
+   res.status(400).send('HUUTAI CI/CD fix issue');
 });
 app.use('/api', router);
 
